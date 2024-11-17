38a9b2fb64a514d4413ab7f7e65bc8fc23d3246f
Neverhood <just7500@gmail.com>
2024-11-17 15:20:32 +0800

    OK =))

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,13 +5,45 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
 
+        int floorAmount = 5;
+        int entranceAmount = 4;
+        int flatNumber = 20;
+
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        String textMessage = flatNumber + " кв - ";
+
+        if (flatNumber < 1 || flatNumber > floorAmount * entranceAmount * 4) {
+            textMessage = "Такой квартиры не существует!";
+            return textMessage;
+        }
+
+        int flatEntrance = (flatNumber + floorAmount * 4 - 1) / (floorAmount * 4);
+        int flatFloor = (flatNumber + 3) / 4 - ((flatEntrance - 1) * floorAmount);
+        int flatCorner = flatNumber % 4;
+
+        textMessage = textMessage + flatEntrance + " подъезд, " + flatFloor + " этаж, ";
+
+        switch (flatCorner) {
+            case (1):
+                textMessage = textMessage + "слева от лифта, влево";
+                break;
+            case (2):
+                textMessage = textMessage + "слева от лифта, вправо";
+                break;
+            case (3):
+                textMessage = textMessage + "справа от лифта, влево";
+                break;
+            case (0):
+                textMessage = textMessage + "справа от лифта, вправо";
+                break;
+
+        }
+
+        return textMessage;
     }
 }
